6df8ac9311757905be1f77828f5e19c304c0b135
Peter Phe <peterphe@gmail.com>
2020-01-10 17:59:25 -0800

    Complete

---
+++ src/main/java/net/ddns/slackercentral/BootReactFullstackTutorialApplication.java
@@ -0,0 +1,13 @@
+package net.ddns.slackercentral;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootReactFullstackTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootReactFullstackTutorialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/ddns/slackercentral/BootReactFullstackTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package net.ddns.slackercentral;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootReactFullstackTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
